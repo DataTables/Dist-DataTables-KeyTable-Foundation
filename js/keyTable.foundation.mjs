49c2e8709ecf481dd3502653d95579c7ca453edb
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:47 +0000

    Add support for mjs files (ES Modules)

---
+++ js/keyTable.foundation.mjs
@@ -0,0 +1,13 @@
+
+/*! Foundation styling wrapper for KeyTable
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-zf';
+import DataTable from 'datatables.net-keytable';
+
+
+
+
+export default DataTable;
