c2becbf6b58dbbbd471eaf49a8bdb416e3974dbc
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 11:52:03 +0000

    2de7c4a50f823d633cecea052206234aa575c1ba
    Sync to source repo @2de7c4a50f823d633cecea052206234aa575c1ba

--- js/keyTable.foundation.min.mjs
+++ js/keyTable.foundation.min.mjs
@@ -1,1 +1,4 @@
+/*! Foundation styling wrapper for KeyTable
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-zf";import DataTable from"datatables.net-keytable";export default DataTable;
