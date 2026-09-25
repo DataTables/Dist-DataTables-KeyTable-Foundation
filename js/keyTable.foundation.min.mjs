0e177bc0052ca9c9e647122be98f0ff35cd6cdf9
dtbuild <dtbuild@rigil>
2026-09-25 11:21:13 +0000

    fcc1573818f29c93c820fe7dbfd55660435acefe Release 3.1.1
    Sync to source repo @fcc1573818f29c93c820fe7dbfd55660435acefe

--- js/keyTable.foundation.min.mjs
+++ js/keyTable.foundation.min.mjs
@@ -1,4 +1,4 @@
-/*! KeyTable Foundation styling 3.1.0 for DataTables
+/*! KeyTable Foundation styling 3.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-zf";import KeyTable from"datatables.net-keytable";export default DataTable;
